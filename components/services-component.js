3ca4ed166fc6b4d1fe14ae6d42be511acc52baa3
MaLynda Poulsen <poulsenjones@yahoo.com>
2016-09-18 16:46:36 -0600

    kjb

--- components/services-component.js
+++ components/services-component.js
@@ -2,8 +2,23 @@
     angular.module('Kelly')
         .component('servicesComponent', {
             templateUrl: 'components/services-component.html',
-          
+          controller: ServiceController,
         })
+    function ServiceController() {
+        var sc = this;
 
+        $.fn.extend({
+            animateCss: function (animationName) {
+                var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+                $(this).addClass('animated ' + animationName).one(animationEnd, function () {
+                    $(this).removeClass('animated ' + animationName);
+                });
+            }
+        });
+
+    $('#scroller').animateCss('flash');
+
+
+    }
 
 }());
