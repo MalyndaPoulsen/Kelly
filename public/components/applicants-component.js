c75321a15f9bdbefc1808a68f4b217bfed9d5aa2
MaLynda Poulsen <poulsenjones@yahoo.com>
2016-12-17 16:29:44 -0700

    remove

--- public/components/applicants-component.js
+++ public/components/applicants-component.js
@@ -18,10 +18,10 @@
             update();
               
         });
-       removeApplicant(function(index){
+       function removeApplicant(index){
             ac.applicant.splice(index, 1);
 
-        });
+        };
    
     }
 
