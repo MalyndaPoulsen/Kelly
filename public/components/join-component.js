2fc44bbc3eadadd1b58034d0d7a8545fae2e806b
MaLynda Poulsen <poulsenjones@yahoo.com>
2016-12-17 15:08:19 -0700

    change models

--- public/components/join-component.js
+++ public/components/join-component.js
@@ -13,15 +13,64 @@
     //         }
     //     }
     // }
-        function JoinController(AuthService) {
+    function JoinController(AuthService) {
         const jc = this;
         jc.join = function (user) {
-            if(user){
+            if (user) {
                 user.applied = Date.now();
                 AuthService.join(user);
-                 console.log("Your application was submitted. Thank you.");
+                console.log("Your application was submitted. Thank you.");
             }
         }
+        jc.user = {
+            name: '',
+            email: '',
+            address: '',
+            phone: ''
+        }
+        jc.user.education = {
+            degree: '',
+            college: '',
+            address: '',
+            degree2: '',
+            college2: '',
+            address2: '',
+        }
+        jc.user.employment = {
+            employer: '',
+            dates: '',
+            enddates: '',
+            supervisor: '',
+            tel: '',
+            employer2: '',
+            dates2: '',
+            enddates2: '',
+            supervisor2: '',
+            tel2: '',
+            employer3: '',
+            dates3: '',
+            enddates3: '',
+            supervisor3: '',
+            tel3: '',
+        }
+        jc.user.contact = {
+            name: '',
+            phone: '',
+            email: '',
+            relationship: '',
+            name2: '',
+            phone2: '',
+            email2: '',
+            relationship2: '',
+            name3: '',
+            phone3: '',
+            email3: '',
+            relationship3: '',
+        }
+        jc.user.resume = ''
 
 
-} }());
+
+
+    }
+} ());
